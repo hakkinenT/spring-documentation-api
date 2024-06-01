c30e13b1901647896544748157c94d176e956cfc
Tawanna <thol_16@hotmail.com>
2024-06-01 12:21:14 -0300

    api doc security

--- src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -58,6 +59,7 @@ public class MovieController {
 		return service.findById(id);
 	}
 
+
 	@Operation(
 			description = "Create a new movie",
 			summary = "Create a new movie",
@@ -69,6 +71,7 @@ public class MovieController {
 					@ApiResponse(description = "Unprocessable Entity", responseCode = "422")
 			}
 	)
+	@SecurityRequirement(name = "bearerAuth")
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping(produces = "application/json")
 	public ResponseEntity<MovieDTO> insert(@Valid @RequestBody MovieDTO dto) {
@@ -89,6 +92,7 @@ public class MovieController {
 					@ApiResponse(description = "Unprocessable Entity", responseCode = "422")
 			}
 	)
+	@SecurityRequirement(name = "bearerAuth")
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PutMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<MovieDTO> update(@PathVariable Long id, @Valid @RequestBody MovieDTO dto) {
@@ -108,6 +112,7 @@ public class MovieController {
 					@ApiResponse(description = "Unprocessable Entity", responseCode = "422")
 			}
 	)
+	@SecurityRequirement(name = "bearerAuth")
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@DeleteMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<MovieDTO> delete(@PathVariable Long id) {
